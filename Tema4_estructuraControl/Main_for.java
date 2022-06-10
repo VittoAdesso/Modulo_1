6dbf646e024aef54c4ad131e8009ef0d1ed7725c
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-10 19:16:10 +0200

    for

---
+++ Tema4_estructuraControl/Main_for.java
@@ -0,0 +1,9 @@
+package Tema4_estructuraControl;
+
+public class Main_for {
+    public static void main_for(String[] args) {
+         for (int numeroFor = 3; numeroFor <= 3; numeroFor++) {
+        System.out.println(numeroFor);
+        }  
+    }
+}
