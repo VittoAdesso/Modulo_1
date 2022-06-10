f84263658fdd4c4056d8ac21c73bc661ff952b20
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-10 19:31:31 +0200

    do while

---
+++ Tema4_estructuraControl/Main_doWhile.java
@@ -0,0 +1,16 @@
+package Tema4_estructuraControl;
+
+public class Main_doWhile {
+        public static void main_doWhile(String[] args) {
+            int numWhile = 0;
+    
+            do  {
+                numWhile++;
+                System.out.println(numWhile);
+            }  
+            
+            while(numWhile < 3);
+        }
+    
+    
+}
